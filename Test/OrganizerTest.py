565db4a3764774abdc842ab991efe5d12b818f47
Daniel Wendt <daniel.wendt@wendt-basis.de>
2024-08-05 10:16:30 +0200

    add test_sort test

--- Test/OrganizerTest.py
+++ Test/OrganizerTest.py
@@ -11,6 +11,7 @@ class OrganizerTest(unittest.TestCase):
     # Create a temporary directory
     self.test_dir = tempfile.TemporaryDirectory()
     self.download_path = self.test_dir.name
+    self.organizer = Organizer(self.download_path)
 
     # Create test files
     self.test_files = {
@@ -37,23 +38,29 @@ class OrganizerTest(unittest.TestCase):
     self.test_dir.cleanup()
 
   def test_create_folders(self):
-    organizer = Organizer(self.download_path)
-    organizer._Organizer__create_folders()
+    self.organizer._Organizer__create_folders()
 
-    for folder in organizer.directories.keys():
+    for folder in self.organizer.directories.keys():
       folder_path = Path(self.download_path) / folder
       self.assertTrue(folder_path.exists(), f"Directory {folder} was not created")
 
+  def test_sort(self):
+    self.organizer._Organizer__create_folders()
+    self.organizer._Organizer__sort()
+
+    for file_name, folder in self.test_files.items():
+      expected_path = Path(self.download_path) / folder / file_name
+      self.assertTrue(expected_path.exists(), f"{file_name} was not moved to {folder} folder")
+
   def test_organize(self):
-    organizer = Organizer(self.download_path)
-    organizer.organize()
+    self.organizer.organize()
 
     for file_name, folder in self.test_files.items():
       expected_path = Path(self.download_path) / folder / file_name
       self.assertTrue(expected_path.exists(), f"{file_name} was not moved to {folder} folder")
 
     # Check that the directories are created
-    for folder in organizer.directories.keys():
+    for folder in self.organizer.directories.keys():
       folder_path = Path(self.download_path) / folder
       self.assertTrue(folder_path.exists(), f"Directory {folder} was not created")
 
